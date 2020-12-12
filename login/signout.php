7772dcf8f11cf66906b62c9c9a85b1def521796f
Ashutosh Singh <sashutosh705@gmail.com>
2020-12-12 08:59:22 +0530

    Add files via upload

---
+++ login/signout.php
@@ -0,0 +1,12 @@
+<?php
+# @Author: Ashutosh Singh <ashutosh>
+# @Date:   2020-10-15T16:35:07+05:30
+# @Last modified by:   ashutosh
+# @Last modified time: 2020-11-10T23:45:04+05:30
+
+
+
+session_destroy();
+
+header('location:/TourMate/index.html');
+?>
